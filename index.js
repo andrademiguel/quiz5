a69ddfadb7629f431014b36edd934a9ba22fd50c
andrademiguel <miguel4andrade@gmail.com>
2020-12-05 17:37:26 -0800

    index

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 const express = require("express");
 const app = express();
 const dblib = require("./dblib.js");
-
+const path = require("path");
 const multer = require("multer");
 const upload = multer();
 
-// Add middleware to parse default urlencoded form
 app.use(express.urlencoded({ extended: false }));
+// npm start
+
 
 // Setup EJS
 app.set("view engine", "ejs");
 
-// Enable CORS (see https://enable-cors.org/server_expressjs.html)
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
@@ -34,20 +34,41 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
-
 app.get("/", async (req, res) => {
-    // Omitted validation check
+
     const totRecs = await dblib.getTotalRecords();
+
+    const car = {
+        carvin: "",
+        carmake: "",
+        carmodel: "",
+        carmileage: ""
+    };
     res.render("index", {
         type: "get",
-        totRecs: totRecs.totRecords
+        totRecs: totRecs.totRecords,
+        car: car
     });
 });
 
+// app.post("/", upload.array(), async (req, res) => {
+//     dblib.findProducts(req.body)
+//         .then(result => res.send(result))
+//         .catch(err => res.send({ trans: "Error", result: err.message }));
+
+// });
+
+// app.get("/", async (req, res) => {
+//     // Omitted validation check
+//     const totRecs = await dblib.getTotalRecords();
+//     res.render("index", {
+//         type: "get",
+//         totRecs: totRecs.totRecords
+//     });
+// });
+
 app.post("/", async (req, res) => {
-    // Omitted validation check
-    //  Can get this from the page rather than using another DB call.
-    //  Add it as a hidden form value.
+
     const totRecs = await dblib.getTotalRecords();
 
     dblib.findProducts(req.body)
@@ -67,28 +88,4 @@ app.post("/", async (req, res) => {
                 car: req.body
             });
         });
-});
-
-app.get("/", async (req, res) => {
-
-    const totRecs = await dblib.getTotalRecords();
-
-    const car = {
-        carvin: "",
-        carmake: "",
-        carmodel: "",
-        carmileage: ""
-    };
-    res.render("index", {
-        type: "get",
-        totRecs: totRecs.totRecords,
-        car: car
-    });
-});
-
-app.post("/", upload.array(), async (req, res) => {
-    dblib.findProducts(req.body)
-        .then(result => res.send(result))
-        .catch(err => res.send({ trans: "Error", result: err.message }));
-
 });
